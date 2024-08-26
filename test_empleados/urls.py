cfb8a1959d540ae2d9f9c6640ee1d04e6f8a8b9c
KevinLopezAvila <keflopavi2001@gmail.com>
2024-08-25 18:37:04 -0600

    correcion de errores

--- test_empleados/urls.py
+++ test_empleados/urls.py
@@ -14,15 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("empleados/", include('empleados.urls'))
+    path("", include('empleados.urls'))
 ]
 
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, # type: ignore
+    urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
